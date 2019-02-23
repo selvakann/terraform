ff999a275c0a0028242c84e18d943e29b608d5ed
selvakann <selvakann@gmail.com>
2019-02-23 19:22:10 +0530

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -2,7 +2,7 @@
 resource "aws_security_group" "web" {
   name        = "web-access"
   description = "Security group to allow access from mgmt vpc"
-  vpc_id      = "vpc-e57e399d"
+  vpc_id      = "vpc-0ca3acea0bc859b82"
    tags {
     Name = "web-access"
   }
